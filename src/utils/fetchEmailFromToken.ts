47974768891f15a1ff047e404dfb6e4257f183dd
31n5ang <31n5ang@gmail.com>
2025-03-29 02:54:02 +0900

    refactor: 로그 삭제

--- src/utils/fetchEmailFromToken.ts
+++ src/utils/fetchEmailFromToken.ts
@@ -9,7 +9,6 @@ const fetchEmailFromToken = async () => {
             const refreshResponse = await instance.post('/api/auth/token/refresh');
             const newAccessToken = refreshResponse.headers['minimalest-access-token'];
             localStorage.setItem('accessToken', newAccessToken);
-            console.log('AccessToken을 임의적으로 재발급 받았음');
             accessToken = newAccessToken;
         } catch (refreshError) {
             console.error('AccessToken 재발급 실패: ', refreshError);
