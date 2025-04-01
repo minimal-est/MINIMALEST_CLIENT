0fe58008535378c88009544c3bffdeca93e76c8e
31n5ang <31n5ang@gmail.com>
2025-04-01 21:58:49 +0900

    feat: 포스트 작성하기 버튼 추가

--- src/pages/ArchiveMain.tsx
+++ src/pages/ArchiveMain.tsx
@@ -1,9 +1,12 @@
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import ArchiveLayout from "../layouts/ArchiveLayout.tsx";
 import PostPreviewsContainer from "../components/Post/PostPreviewsContainer.tsx";
 import RepresentativePostViewContainer from "../components/Post/RepresentativePostViewContainer.tsx";
 import styled from "styled-components";
 import {Helmet} from "react-helmet-async";
+import {useEffect, useState} from "react";
+import validateFromEmailToken from "../utils/validateFromEmailToken.ts";
+import Button from "../components/Button/Button.tsx";
 
 const StyledContainer = styled.div`
     display: flex;
@@ -19,6 +22,22 @@ const StyledRecentPostPreviews = styled.div`
 `;
 
 const ArchiveMain = () => {
+    const [isAdminMode, setIsAdminMode] = useState(false);
+    const [isMemberLoading, setIsMemberLoading] = useState<boolean>(true);
+    const [isMember, setIsMember] = useState<boolean>(false);
+    // const [emailIfMember, setEmailIfMember] = useState<string | undefined>("");
+
+    const updateIsMember = async () => {
+        const memberValidation = await validateFromEmailToken();
+        setIsMember(memberValidation.isValid);
+        // setEmailIfMember(memberValidation.email);
+        setIsAdminMode(memberValidation.isValid);
+        setIsMemberLoading(false);
+    }
+
+    useEffect(() => {
+        updateIsMember();
+    }, []);
 
     const params = useParams();
     const author = params.author ?? "";
@@ -33,6 +52,11 @@ const ArchiveMain = () => {
             </Helmet>
 
             <StyledContainer>
+                {!isMemberLoading && isMember && isAdminMode && (
+                    <Link to={`/archive/${author}/create`}>
+                        <Button type='button' size='medium' color='black'>포스트 작성하기 ✍🏻</Button>
+                    </Link>
+                )}
                 <RepresentativePostViewContainer author={author} />
                 <StyledRecentPostPreviews>
                     <PostPreviewsContainer author={author} />
